3c90e810c40105d89d4dd5ae9bfc031be60a402c
Neco_Stack <necoabay@googlemail.com>
2024-09-28 13:22:57 +0200

    intro ts first task

--- src/basics_ts/basic.ts
+++ src/basics_ts/basic.ts
@@ -1,4 +1,95 @@
-console.log("welcome");
-console.log("Hallo necati");
+// let dogName: string = "Leon"
+// console.log('dogName');
+// console.log(dogName);
+// console.log("Necati");
+// console.log(dogName);
+// dogName = "elf"
+
+// let a = 1 + 2
+// let b = a + 3 
+// let c = {
+//     apple : a, 
+//     banana : b
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function squareOf(n){
+//     return n * n
+// }
+// squareOf (3)
+// console.log(squareOf);
+// console.log(3);
+// squareOf ('z');
+// console.log("z");
+
+// function squareOf(n: number){
+//     return n * n 
+// }
+// let n = 2;
+// squareOf(n);
+// console.log(n);
+// squareOf(2);
+// function myNamedFn():string{
+//     return "hi";
+// }
+// myNamedFn();
+// let name= "necati"
+// let selection: string = 'Schere';
+// let enemySelection:string = 'Stein;'
+// console.log('Deine Auswahl:  ' + selection);
+// console.log('Dein Gegner: ' + enemySelection);
+// // enemySelection = hiermit kannst du den Gegner deklarieren
+
+// // mit : typ: number,BigInt,Boolean,String,Symbol,Object
+// // wir  müssen den Typ des Parameters explizit 
+// // annotieren: let name: string = ""; 
+// // damit keine anderen Typen akzeptiert werden
+// console.log(23);
+// INTRO-TS-Level-1_12
+let dogName = 'Leon';
+console.log(dogName);
+dogName = 'elf';
+console.log(dogName);
+
+let seventeen:number = 17;
+const sum = seventeen + 44;
+console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
